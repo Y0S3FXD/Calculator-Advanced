0f444d23a182162ed743740b0d3640c4fc050cfd
Y0S3FXD <shekyousef@gmail.com>
2023-08-23 09:54:58 +0200

    Fixed the code more
    FIxed the code more and dropped the idea of using plugins, it was a bit problematic, so since it was just a calc it was ok. but it would be fun. and now using visual studio professional instead. and now we got the calc to work simply. but not tottally

--- Assets/Scripts/Calculator.cs
+++ Assets/Scripts/Calculator.cs
@@ -1,13 +1,67 @@
-using System.Linq.Expressions;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using NCalc;
 
 public class Calculator : MonoBehaviour
 {
-    public string Calculate(string equation){
-        Expression expression = new Expression(equation);
-        return expression.Evaluate().ToString();
+    #region fields
+    public TextMeshProUGUI inputtext ;
+    private float _result;
+    private float _input1;
+    private float _input2;
+    private string _operation;
+    #endregion fields
+
+
+    #region methods
+    void Start()
+    {
+
     }
+    public void ClickNumber(int val)
+    {
+        Debug.Log($"check val: {val}");
+        inputtext.text = $"{val}";
+        if (_input1 == 0)
+        {
+            _input1 = val;
+        }
+        else
+        {
+            _input2 = val;
+        }
+    }
+    public void ClickOperator(string val)
+    {
+        Debug.Log($"clickoper val: {val}");
+        _operation = val ;
+    }
+    public void ClickEqual(string val)
+    {
+        Debug.Log($"clickequal val: {_result}");
+        if (_input1 != 0 &&  _input2 != 0 && !string.IsNullOrEmpty(_operation) )
+            {
+        switch (_operation)
+            {
+                case "+":
+                _result = _input1 + _input2; break;
+                case "-":
+                _result = _input1 - _input2; break;
+                case "*":
+                    _result = _input1 * _input2; break;
+                    case "/":
+                    _result = _input1 / _input2;
+                    break;
+                        inputtext.SetText(_result.ToString());
+            }
+        }
+    }
+    public void ClickPeriod (string val){
+        Debug.Log($"clickperiod val: {val}");
+    }
+    #endregion methods
+
+    #region events
+    #endregion events
 }
